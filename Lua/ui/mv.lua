badb69208b9cef193670a03f17b7cdd55fc93179
eerrly <chenerlishuaige@163.com>
2023-11-03 15:27:08 +0800

    添加注释

--- Lua/ui/mv.lua
+++ Lua/ui/mv.lua
@@ -1,4 +1,5 @@
 return {
+    -- Lua类                 弹窗类型             父窗口Lua类           对应的Model类           对应的Prefab
     { "viewa",              "normal",             nil,                "modela",             "A" },
     { "viewb",              "popup",              nil,                "modela",             "B" },
     { "viewc",              "popup",              nil,                "modela",             "C" },

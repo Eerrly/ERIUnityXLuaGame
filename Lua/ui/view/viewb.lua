461052a30e6fb9116d4fb3439512e0bf9ce2f3db
eerrly <chenerlishuaige@163.com>
2025-04-14 18:26:33 +0800

    测试热更

--- Lua/ui/view/viewb.lua
+++ Lua/ui/view/viewb.lua
@@ -11,7 +11,7 @@ function viewB:OnCreate()
 end
 
 function viewB:RefreshView()
-    self.View:SetText(self.ID.Text_Desc, "Hello World.")
+    self.View:SetText(self.ID.Text_Desc, "Fuck The World.")
 end
 
 function viewB:BindEvent()
